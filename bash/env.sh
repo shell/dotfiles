75ee3e83d3373d694d3dffd493249f31a7e66d23
Vladimir <penkinv@gmail.com>
2011-02-12 01:32:46 +0300

    *  Minimal todo and tip commands (http://blog.jerodsanto.net/2010/12/minimally-awesome-todos/)

--- bash/env.sh
+++ bash/env.sh
@@ -1,4 +1,7 @@
 export EDITOR=mate      
+export TODO=~/Documents/todo
+export TIP=~/Documents/tip
 export blog=/Users/vladimirpenkin/Sites/penkin.co.uk
 
 if [[ -s /Users/vladimirpenkin/.rvm/scripts/rvm ]] ; then source /Users/vladimirpenkin/.rvm/scripts/rvm ; fi
+
--- bash/functions.sh
+++ bash/functions.sh
@@ -3,3 +3,9 @@
 function mkcd {
   mkdir -p "$1" && cd "$1"
 }
+
+function todo() { if [ $# == "0" ]; then cat $TODO; else echo "• $@" >> $TODO; fi }
+
+function todone() { sed -i -e "/$*/d" $TODO; }
+
+function tip() { if [ $# == "0" ]; then cat $TIP; else echo "• $@" >> $TIP; fi }
